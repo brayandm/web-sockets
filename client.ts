ec89bae316816ccfa21ce3f868318f2e26abfbd3
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-24 00:57:43 +0200

    client is typescript now

--- client.ts
+++ client.ts
@@ -1,11 +1,10 @@
-const WebSocket = require('ws');
+import WebSocket from 'ws';;
 
 const ws = new WebSocket('ws://127.0.0.1:8080/', {
   perMessageDeflate: false
 });
 
-
-// ws.on('error', console.error);
+ws.on('error', console.error);
 
 ws.on('open', function open() {
   ws.send('something');
--- server.ts
+++ server.ts
@@ -1,16 +1,14 @@
 import WebSocket from 'ws';
 
-// Crea un nuevo servidor WebSocket en el puerto 8080
 const server = new WebSocket.Server({ port: 8080 });
 
-// Manejador de conexión para el servidor WebSocket
-server.on('connection', (socket) => {
-  // Maneja los mensajes recibidos desde el cliente
-  socket.on('message', (message) => {
-    console.log('Mensaje recibido:', message);
+server.on('connection', (connection) => {
+
+  connection.on('message', (message) => {
+    console.log('Message received:', message);
     // Envía un mensaje de respuesta al cliente
-    socket.send('Respuesta del servidor: ' + message);
+    connection.send('Answer from server: ' + message);
   });
 });
 
-console.log('Servidor WebSocket iniciado en el puerto 8080');
+console.log('Server running on port 8080');
