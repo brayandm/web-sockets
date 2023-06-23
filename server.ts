32eab898c4a99401a5c2782bc2bf3d414a7ba0d8
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-24 01:11:11 +0200

    upd

--- server.ts
+++ server.ts
@@ -1,14 +1,22 @@
 import * as dotenv from "dotenv";
 import WebSocket from "ws";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
 const server = new WebSocket.Server({ port: Number(process.env.PORT!) });
 
+const clients: { [key: string]: WebSocket } = {};
+
 server.on("connection", (connection) => {
+  const userId = uuidv4();
+
+  console.log(`Received a new connection: ` + userId);
+
+  clients[userId] = connection;
+
   connection.on("message", (message) => {
     console.log("Message received:", message);
-    // Envía un mensaje de respuesta al cliente
     connection.send("Answer from server: " + message);
   });
 });
